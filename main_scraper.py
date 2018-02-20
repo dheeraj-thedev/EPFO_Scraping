d7a5d3bf6f17d587a4ec40470361f520fb5b258b
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2018-02-20 13:06:02 +0530

    adding .gitignore

--- main_scraper.py
+++ main_scraper.py
@@ -8,10 +8,9 @@ def generate_and_read_captcha(img_src):
     my_url = BASE_URL + img_src
     response = requests.get(my_url, stream=True)
     filename = "current_captcha.png"
-    with open(filename, "wb") as f:  # I know that "wb" and "w" do the same
-        # thing on mac, this is for universality
+    with open(filename,
+              "wb") as f:  # I know that "wb" and "w" do the same thing on mac, this is for universality
         copyfileobj(response.raw, f)
     del response
     return decode(filename)
-
 
